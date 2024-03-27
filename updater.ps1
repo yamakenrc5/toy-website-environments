4b37915200e7cdaa11b4c9b6e44c2b63b37df1bb
Kenji Yamamoto <kenji.yamamoto1@nnlife.co.jp>
2024-03-27 10:06:12 +0900

    amend deploy.yml at 2024-3月-27, 10:06:11

--- updater.ps1
+++ updater.ps1
@@ -2,7 +2,7 @@
 # file name, version, description, usage, design tips
 # params and vars
 $datetime = (Get-Date -Format 'yyyy-MMM-dd, HH:mm:ss')
-$msgbody = 'Add reusable workflows'
+$msgbody = 'amend deploy.yml'
 $commitmessage = "$msgbody at $datetime"
 $addtorepository = '.' #'deploy/azure-pipelines.yml'
 #command body
