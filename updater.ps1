587fed47f49189fb452f3e62358011a3ad9a7cd1
Kenji Yamamoto <kenji.yamamoto1@nnlife.co.jp>
2024-03-28 17:33:32 +0900

    amend lint.yml at 2024-3月-28, 17:33:32

--- updater.ps1
+++ updater.ps1
@@ -2,7 +2,7 @@
 # file name, version, description, usage, design tips
 # params and vars
 $datetime = (Get-Date -Format 'yyyy-MMM-dd, HH:mm:ss')
-$msgbody = 'amend deploy.yml'
+$msgbody = 'amend lint.yml'
 $commitmessage = "$msgbody at $datetime"
 $addtorepository = '.' #'deploy/azure-pipelines.yml'
 #command body
