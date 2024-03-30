98f261fac248b883b6296f7db62712b567040e43
Kenji Yamamoto <kenji.yamamoto1@nnlife.co.jp>
2024-03-30 21:16:20 +0900

    updating smoke-test step, script path with single quote, added ./ at 2024-3月-30, 21:16:20

--- updater.ps1
+++ updater.ps1
@@ -2,7 +2,7 @@
 # file name, version, description, usage, design tips
 # params and vars
 $datetime = (Get-Date -Format 'yyyy-MMM-dd, HH:mm:ss')
-$msgbody = 'updating smoke-test step, script path with single quote, removed ./'
+$msgbody = 'updating smoke-test step, script path with single quote, added ./'
 $commitmessage = "$msgbody at $datetime"
 $addtorepository = '.' #'deploy/azure-pipelines.yml'
 #command body
