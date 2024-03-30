5c9c0b7d898c79ae9647927996e51d11bc16f3ef
Kenji Yamamoto <kenji.yamamoto1@nnlife.co.jp>
2024-03-30 17:03:55 +0900

    updating smoke-test step at 2024-3月-30, 17:03:55

--- updater.ps1
+++ updater.ps1
@@ -2,7 +2,7 @@
 # file name, version, description, usage, design tips
 # params and vars
 $datetime = (Get-Date -Format 'yyyy-MMM-dd, HH:mm:ss')
-$msgbody = 'consolidate steps into workflow.yml because when service principal is used, there are challenges about variables'
+$msgbody = 'updating smoke-test step'
 $commitmessage = "$msgbody at $datetime"
 $addtorepository = '.' #'deploy/azure-pipelines.yml'
 #command body
