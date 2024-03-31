6aa0385d93817ee6d0d36a6dbdaa200df52098c6
Kenji Yamamoto <kenji.yamamoto1@nnlife.co.jp>
2024-03-31 17:33:40 +0900

    removed some unnecessary spaces at 2024-3月-31, 17:33:40

--- updater.ps1
+++ updater.ps1
@@ -2,7 +2,7 @@
 # file name, version, description, usage, design tips
 # params and vars
 $datetime = (Get-Date -Format 'yyyy-MMM-dd, HH:mm:ss')
-$msgbody = 'moved variables and secrets in the YML to the top position at each step, added parameters and modified resource in main.bicep'
+$msgbody = 'removed some unnecessary spaces'
 $commitmessage = "$msgbody at $datetime"
 $addtorepository = '.' #'deploy/azure-pipelines.yml'
 #command body
