fd5713057bdbe29a446bb4aae548a86bde4b4bae
Kenji Yamamoto <kenji.yamamoto1@nnlife.co.jp>
2024-03-31 19:48:50 +0900

    Try to resolve 'Error: Login failed with Error: Using auth-type: SERVICE_PRINCIPAL. Not all values are present. Ensure 'client-id' and 'tenant-id' are supplied.. Double check if the 'auth-type' is correct. Refer to https://github.com/Azure/login#readme for more information.' at 2024-3月-31, 19:48:50

--- updater.ps1
+++ updater.ps1
@@ -3,7 +3,7 @@
 # params and vars
 $datetime = (Get-Date -Format 'yyyy-MMM-dd, HH:mm:ss')
 $msgbody = 'relocating variables'
-$commitmessage = "Try to resolve 'Error: Deployment process failed as some lines were written to stderr' at $datetime"
+$commitmessage = "Try to resolve 'Error: Login failed with Error: Using auth-type: SERVICE_PRINCIPAL. Not all values are present. Ensure 'client-id' and 'tenant-id' are supplied.. Double check if the 'auth-type' is correct. Refer to https://github.com/Azure/login#readme for more information.' at $datetime"
 $addtorepository = '.' #'deploy/azure-pipelines.yml'
 #command body
 git add $addtorepository
