738b0b0d0b1bae83ebce7eb00bd2d74191d916a4
Kenji Yamamoto <kenji.yamamoto1@nnlife.co.jp>
2024-03-31 18:28:13 +0900

    relocating variables at 2024-3月-31, 18:28:13

--- updater.ps1
+++ updater.ps1
@@ -2,7 +2,7 @@
 # file name, version, description, usage, design tips
 # params and vars
 $datetime = (Get-Date -Format 'yyyy-MMM-dd, HH:mm:ss')
-$msgbody = 'removed some unnecessary spaces'
+$msgbody = 'relocating variables'
 $commitmessage = "$msgbody at $datetime"
 $addtorepository = '.' #'deploy/azure-pipelines.yml'
 #command body
