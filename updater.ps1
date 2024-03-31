0378927a4e91eee35edc59fe069e31fbcca09f7a
Kenji Yamamoto <kenji.yamamoto1@nnlife.co.jp>
2024-03-31 15:09:59 +0900

    moved variables and secrets in the YML to the top position at each step, added parameters and modified resource in main.bicep at 2024-3月-31, 15:09:59

--- updater.ps1
+++ updater.ps1
@@ -2,7 +2,7 @@
 # file name, version, description, usage, design tips
 # params and vars
 $datetime = (Get-Date -Format 'yyyy-MMM-dd, HH:mm:ss')
-$msgbody = 'trimmed env and inputs.'
+$msgbody = 'moved variables and secrets in the YML to the top position at each step, added parameters and modified resource in main.bicep'
 $commitmessage = "$msgbody at $datetime"
 $addtorepository = '.' #'deploy/azure-pipelines.yml'
 #command body
