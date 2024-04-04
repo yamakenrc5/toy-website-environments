c48a1202c824ffb999f6dc595ee8ea748b0633da
Kenji Yamamoto <kenji.yamamoto1@nnlife.co.jp>
2024-04-05 00:49:58 +0900

    commit

--- removeresource.ps1
+++ removeresource.ps1
@@ -2,3 +2,5 @@
 $target=(Get-AzResourceGroup) | Where-Object { $_.ResourceGroupName -like "*ToyWebsite*" } | Select-Object -ExpandProperty ResourceGroupName
 
 ForEach ($rg in $target) {Remove-AzResourceGroup -Name $rg -Force} #-whatif}
+
+# az group delete -n $rg --no-wait
